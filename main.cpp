ae413254d4ba207a3c8ed374562579f480e0cda6
mstevz <dev.mstevz@gmail.com>
2020-11-29 06:05:10 +0000

    Updates main and Visual Studio Project Settings

--- main.cpp
+++ main.cpp
@@ -4,8 +4,7 @@
 
 #include "Pong.hpp"
 
-
-void PongRun() {
+int main(){   
     Pong game;
 
     game.setGameMode(GameMode::VersusAI);
@@ -16,49 +15,9 @@ void PongRun() {
     catch (std::exception e) {
         std::cout << e.what();
     }
-}
-
-void Test() {
-    sf::Text dt;
-    sf::Text fps;
-    sf::Font Arial;
-
-    sf::RenderWindow window(sf::VideoMode(800, 480), "Pong");
-
-    if (!Arial.loadFromFile("./assets/fonts/calibri.ttf"))
-    {
-        throw std::exception("[EXCEPTION] Could not load Arial font!");
-    }
-
-    dt.setPosition(10.f, 5.f);
-    dt.setCharacterSize(12);
-    dt.setFont(Arial);
-    dt.setFillColor(sf::Color::White);
-    dt.setString("Hello World!");
-    sf::Event ev;
-
-    while (window.isOpen()) {
-        while (window.pollEvent(ev)) {
-            switch (ev.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            }
-        }
-        window.clear();
-        window.draw(dt);
-        window.display();
-    }
-}
-
-int main(){   
-  
-    //Test();
-
-    PongRun();
-   
-   
-    
 
     return 0;
 }
+
+
+
